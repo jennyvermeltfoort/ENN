ff1f837797384634bef7127d8d3145fa2496d0bb
Jenny Vermeltfoort <jennyvermeltfoort@outlook.com>
2022-01-08 11:43:47 +0100

    clang-format: indent to 4.

--- src/chip/stm32/i2c/i2c.c
+++ src/chip/stm32/i2c/i2c.c
@@ -9,22 +9,20 @@ void i2c_init(void) { printf("i2c init.\n"); }
 
 void i2c_read(uint8_t size, uint8_t *data)
 {
-  (void)size;
-  (void)data;
+    (void)size;
+    (void)data;
 
-  uint16_t test = 0;
-  uint16_t test2 = 0;
+    uint16_t test = 0;
+    uint16_t test2 = 0;
 
+    test |= I2C_OAR1_ADDMODE_SET(BIT_SET);
+    test2 = I2C_OAR1_ADDMODE_GET(test);
 
-  test |= I2C_OAR1_ADDMODE_SET(BIT_SET);
-  test2 = I2C_OAR1_ADDMODE_GET(test);
-
-
-  printf("%d, %d\n", test, test2);
+    printf("%d, %d\n", test, test2);
 }
 
 void i2c_write(uint8_t size, uint8_t *data)
 {
-  (void)size;
-  (void)data;
+    (void)size;
+    (void)data;
 }
